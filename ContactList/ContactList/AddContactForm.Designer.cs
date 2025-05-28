a36d6e3bbcfa8827e83bda9c82cd46f8829cb0ce
doanhuuhai <haidoan1107@gmail.com>
2025-05-28 09:38:39 +0700

    Thêm Form để thêm liên hệ mới

---
+++ ContactList/ContactList/AddContactForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ContactList
+{
+    partial class AddContactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TextBox = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // TextBox
+            // 
+            TextBox.Location = new Point(167, 61);
+            TextBox.Name = "TextBox";
+            TextBox.Size = new Size(125, 27);
+            TextBox.TabIndex = 0;
+            TextBox.Text = "Ho Ten";
+            TextBox.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(424, 61);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "So Dien Thoai";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(212, 188);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Luu";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // AddContactForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(TextBox);
+            Name = "AddContactForm";
+            Text = "AddContactForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox TextBox;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
